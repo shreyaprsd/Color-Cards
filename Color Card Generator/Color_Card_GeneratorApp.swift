80cd33cc023639ad01a4f5864c0d5c1f31ce9563
Shreya Prasad <prasadshreya2303@gmail.com>
2025-08-01 19:05:51 +0530

    Initial Commit

---
+++ Color Card Generator/Color_Card_GeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Color_Card_GeneratorApp.swift
+//  Color Card Generator
+//
+//  Created by Shreya Prasad on 01/08/25.
+//
+
+import SwiftUI
+
+@main
+struct Color_Card_GeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
