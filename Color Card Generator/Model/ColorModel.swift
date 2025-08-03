d4f9ec50dbb69a6e766fee1040d9f1e87b2f8beb
Shreya Prasad <prasadshreya2303@gmail.com>
2025-08-03 19:05:29 +0530

    Swiftdata changes

--- Color Card Generator/Model/ColorModel.swift
+++ Color Card Generator/Model/ColorModel.swift
@@ -5,15 +5,15 @@
 //  Created by Shreya Prasad on 01/08/25.
 //
 import Foundation
-import SwiftUI
 import SwiftData
+import SwiftUI
 
 @Model
 class ColorModel {
-    var hexCode : String
-    var timeStamp : Date
-    var id = UUID()
-    init(hexCode: String, timeStamp: Date, id : UUID = UUID()) {
+    var hexCode: String
+    var timeStamp: Date
+    @Attribute(.unique) var id = UUID()
+    init(hexCode: String, timeStamp: Date, id: UUID = UUID()) {
         self.hexCode = hexCode
         self.timeStamp = timeStamp
         self.id = id
